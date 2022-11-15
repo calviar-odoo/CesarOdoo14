66451f60bade391bff5fc4889efa32cfec53d1e4
calviar-odoo <calviarpayall@gmail.com>
2022-11-15 16:15:01 -0400

    Subida de desarrollos en Odoo

---
+++ payall/models/account_analytic_line.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from dataclasses import field
+from odoo import models, fields, api
+
+class AccountAnalyticLine( models.Model):
+    _inherit = 'account.analytic.line'
+
+    line_type = fields.Many2one(
+        string = "Tipo Registro",
+        comodel_name = "payall.task.type",
+        help = "Seleccione la opción que describe con mayor precisión la actividad ejecutada"
+    )
+
+
+    def write(self, vals):
+
+        result = super( AccountAnalyticLine, self).write(vals)
+        if 'name' in vals and vals['name'] and result:
+            self.sudo().task_id.message_post( body = 
+                "El usuario " + self.env.user.display_name + " ha modificado la descripcion del registro de horas a " + self.name,
+                message_type = "comment"
+            )
+        if 'date' in vals and vals['date'] and result:
+            self.sudo().task_id.message_post( body = 
+                "El usuario " + self.env.user.display_name + " ha modificado la descripcion del registro de horas a " + str(self.date),
+                message_type = "comment"
+            )
+        if 'unit_amount' in vals and vals['unit_amount'] and result:
+            self.sudo().task_id.message_post( body = 
+                "El usuario " + self.env.user.display_name + " ha modificado las horas realizadas del registro de horas a " +  str(self.unit_amount),
+                message_type = "comment"
+            )
+        if 'employee_id' in vals and vals['employee_id'] and result:
+            self.sudo().task_id.message_post( body = 
+                "El usuario " + self.env.user.display_name + " ha modificado el empleado del registro de horas a " + self.employee_id.name,
+                message_type = "comment"
+            )
